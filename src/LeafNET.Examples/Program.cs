2ab2ef8e8457fa9867623f2ef7df9439cf8ec2c0
nic <bolton21@uwindsor.ca>
2025-09-27 22:56:33 -0400

    add Examples project

--- src/LeafNET.Examples/Program.cs
+++ src/LeafNET.Examples/Program.cs
@@ -4,7 +4,7 @@ using LeafNET.Examples;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<LeafNET.Examples.Components.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
